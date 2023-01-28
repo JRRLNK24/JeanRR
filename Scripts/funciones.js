8a45cb30e0f47366e2f9b97046236b297fc1a250
JRRLNK24 <jeanrrlink@gmail.com>
2023-01-28 11:29:50 -0600

    Agregamos bucles en el apartado de variables

--- Scripts/funciones.js
+++ Scripts/funciones.js
@@ -30,39 +30,94 @@
 
 // console.log(auto.modelo)
 
-function Suma(a,b,c){
-  const resultado= a + b + c;
-  console.log(resultado);
-}
+// function Suma(a,b,c){
+//   const resultado= a + b + c;
+//   console.log(resultado);
+// }
 
-Suma(5,9,4);
+// Suma(5,9,4);
 
-function Comparar (a,b){
-    if (a<b){
-      console.log("Buen analisis");
-    } else if (a == b){
-      console.log("Interesante interpretación")
-    }
-}
-Comparar (10,12);
+// function Comparar (a,b){
+//     if (a<b){
+//       console.log("Buen analisis");
+//     } else if (a == b){
+//       console.log("Interesante interpretación")
+//     }
+// }
+// Comparar (10,12);
 
-const Suma2 = (a,b,c) => console.log(`Los numeros son ${a} y ${b} al igual que ${c} `);
-Suma2(4,16,7);
+// const Suma2 = (a,b,c) => console.log(`Los numeros son ${a} y ${b} al igual que ${c} `);
+// Suma2(4,16,7);
 
-const Comparar2 = (a,b) => {
-  console.log("Vamos a comparar");
-  return a,b ;
-}
-Comparar2(10,12) <= true ? console.log("Buen analisis") : console.log("Interesante interpretación")
+// const Comparar2 = (a,b) => {
+//   console.log("Vamos a comparar");
+//   return a,b ;
+// }
+// Comparar2(10,12) <= true ? console.log("Buen analisis") : console.log("Interesante interpretación")
 
-const Cliente = {
-  Nombre: "Jean",
-  NuevoCliente: "No",
-  Sucursal: "Puebla",
-  TotalPedido: 1580,
-  TipoEntrega: "Domicilio",
+// const Cliente = {
+//   Nombre: "Jean",
+//   NuevoCliente: "No",
+//   Sucursal: "Puebla",
+//   TotalPedido: 1580,
+//   TipoEntrega: "Domicilio",
 
-}
+// }
 
-const {Nombre, NuevoCliente, Sucursal} = Cliente;
-console.log(Nombre, NuevoCliente, Sucursal);
+// const {Nombre, NuevoCliente, Sucursal} = Cliente;
+// console.log(Nombre, NuevoCliente, Sucursal);
+
+// Condicionales 
+
+// const clave=123;
+// if(clave==123){
+//   console.log("Aprobado");
+// } else {
+//   console.log("Denegado");
+// }
+
+// Operador ternario:
+// let acceso = (clave == 123 ? console.log(`Aprobado`) : console.log(`Denegado`));
+
+// let clave = prompt (`Ingrese clave: `);
+
+// let acceso = (clave == 123 ? true : false);
+
+// if (acceso == true){
+//   alert("Acceso aprobado");
+// } else {
+//   alert ("Acceso denegado");
+// }
+
+// Condicional else if:
+
+// let edad = prompt("Ingrese su edad: ");
+
+// if(edad >=60){
+//   alert("Usted es de la tercera edad")
+// } else if (edad >= 18){
+//   alert("Usted es mayor de edad");
+// } else{
+//   alert ("Usted esta chiquito");
+// }
+
+//Bucle for
+
+// for ( variable = valor_inicial ; condición_a_evaluar ; incremento ) {
+//   acción/es a realizar;
+// }
+
+// for (i=1; i <= 5; i++ ){
+//   console.log(`Impresión de mensaje` + i);
+// }
+
+// let estudiantes = [
+//   { nombre: `Arturo`, edad: `23`, carrera: `Ingeniería Mécanica`},
+//   { nombre: `Omar`, edad: `23`, carrera: `Medicina veterinaria`},
+//   { nombre: `Javier`, edad: `22`, carrera: `Telecomunicaciones`},
+//   { nombre: `Jaime`, edad:`24`, carrera: `Negocios`}
+// ];
+
+// for(i = 0; i < estudiantes.lenght; i++){
+//   console.log(`El nombre del estudiante es: ` + estudiantes[i].nombre);
+// }
--- Scripts/variables.js
+++ Scripts/variables.js
@@ -1,28 +1,64 @@
 
-let nombrecliente= prompt("¿Como te llamas?");
-console.log(nombrecliente)
+// let nombrecliente= prompt("¿Como te llamas?");
+// console.log(nombrecliente)
 
-const apellidocliente= prompt("¿Cual es tu apellido?");
-console.log(apellidocliente);
+// const apellidocliente= prompt("¿Cual es tu apellido?");
+// console.log(apellidocliente);
 
-var sucursal = prompt ("¿En que sucursal sera tu pedido? ");
-if (sucursal !="Puebla" || "Merida") {
-    alert("Hola " + nombrecliente +  apellidocliente  + "\nNo tenemos sucursal cerca de ti, por favor ingresa Puebla o Merida.");
-    sucursal = prompt("Ingresa la sucursal Puebla o Merida");
+// var sucursal = prompt ("¿En que sucursal sera tu pedido? ");
+// if (sucursal !="Puebla" || "Merida") {
+//     alert("Hola " + nombrecliente +  apellidocliente  + "\nNo tenemos sucursal cerca de ti, por favor ingresa Puebla o Merida.");
+//     sucursal = prompt("Ingresa la sucursal Puebla o Merida");
+// }
+
+// let Cant_Sand_Frias = 2;
+// let Cant_Sand_Queso = 1;
+// let Cant_Sand_Rey = 0;
+// let Cost_Frias=80;
+// let Cost_Queso=30;
+// let Cost_Rey=30;
+
+// console.log(Cant_Sand_Frias * Cost_Frias);
+
+// console.log(Cant_Sand_Rey=Cost_Rey);
+// console.log(Cant_Sand_Rey+=Cost_Rey);
+
+// console.log(Cant_Sand_Rey*Cost_Rey);
+
+// console.log(Cant_Sand_Rey==Cant_Sand_Queso);
+
+//Bucles
+let edad= prompt("Ingrese su edad")
+let number= (edad >= 18 ? console.log("VALIDO") : console.log("NO VALIDO"));
+
+// Crea un array que contenga mínimo 4 objetos, que a su vez contengan como mínimo 3 claves e imprime todo el array en consola con un bucle “for”. 
+
+const promociones= [
+{id:101, promo:"2x1", aplicable: "Sandwich de queso", sucursal: "Mérida"},
+{id:102, promo:"3x2", aplicable: "Sandwich de helado", sucursal: "Puebla"},
+{id:103, promo:"10% descuento", aplicable: "Compra de +$500", sucursal: "Mérida y Puebla"},
+{id:104, promo:"5% descuento", aplicable: "Cliente frecuente", sucursal: "Puebla"},
+];
+for(let i = 0; i < promociones.lenght; i++){
+console.log("Se aplicara la promo:" , promociones[i]);
 }
 
-let Cant_Sand_Frias = 2;
-let Cant_Sand_Queso = 1;
-let Cant_Sand_Rey = 0;
-let Cost_Frias=80;
-let Cost_Queso=30;
-let Cost_Rey=30;
+// Crear un bucle switch donde simula la venta de entradas al cine. Donde dependiendo de la edad el cliente recibe alguna promoción.
 
-console.log(Cant_Sand_Frias * Cost_Frias);
-
-console.log(Cant_Sand_Rey=Cost_Rey);
-console.log(Cant_Sand_Rey+=Cost_Rey);
-
-console.log(Cant_Sand_Rey*Cost_Rey);
-
-console.log(Cant_Sand_Rey==Cant_Sand_Queso);
+switch(edad){
+    case '18':
+        console.log("Felicidades, tu descuento sera de un 5%");
+       break;
+    case '20':
+        console.log("Felicidades, tu descuento sera de un 10%");
+       break;
+    case '22':
+       console.log("Felicidades, tu descuento sera de un 15%");
+       break;
+    case '24':
+       console.log("Felicidades, tu descuento sera de un 20%");
+       break;
+    default:
+       console. log("Tu edad no aplica para una promoción");
+       break; 
+}
